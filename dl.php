e1bc20cc948a4911a8b24c6463bfe8fe1942d840
Jeff Tilson <jrtilson@gmail.com>
2013-04-17 15:27:27 -0400

    Working m4a's! No more rewrite..

--- dl.php
+++ dl.php
@@ -1,12 +1,66 @@
 <?php
+/** 
+ * Beautiful file reader.. 
+ *
+ * See:
+ * https://groups.google.com/d/msg/jplayer/nSM2UmnSKKA/Hu76jDZS4xcJ
+ * http://php.net/manual/en/function.readfile.php#86244
+ */
 error_reporting(0); ini_set('display_errors', 0);
+
 $file = $_GET['file'];
 $size = filesize($file);
-header("Content-Type: audio/mp4");
+
+$type = $_GET['type'];
+
+if (!file_exists($file)) {
+	header ("HTTP/1.1 404 Not Found");
+	return;
+}
+
+$size	= filesize($file);
+$time	= date('r', filemtime($file));
+
+$fm	= @fopen($file, 'rb');
+if (!$fm) {
+	header ("HTTP/1.1 505 Internal server error");
+	return;
+}
+
+$begin	= 0;
+$end	= $size - 1;
+
+if (isset($_SERVER['HTTP_RANGE'])) {
+	if (preg_match('/bytes=\h*(\d+)-(\d*)[\D.*]?/i', $_SERVER['HTTP_RANGE'], $matches)){
+		$begin	= intval($matches[1]);
+		if (!empty($matches[2])) {
+			$end	= intval($matches[2]);
+		}
+	}
+}
+
+if (isset($_SERVER['HTTP_RANGE'])) {
+	header('HTTP/1.1 206 Partial Content');
+} else {
+	header('HTTP/1.1 200 OK');
+}
+
+header("Content-Type: audio/$type"); 
+header('Cache-Control: public, must-revalidate, max-age=0');
+header('Pragma: no-cache');  
 header('Accept-Ranges: bytes');
-header("Content-Length: " . $size);
-ob_clean();
-flush();
-readfile($file);
-exit;
-?>
+header('Content-Length:' . (($end - $begin) + 1));
+if (isset($_SERVER['HTTP_RANGE'])) {
+	header("Content-Range: bytes $begin-$end/$size");
+}
+header("Content-Disposition: inline; filename=$file");
+header("Content-Transfer-Encoding: binary");
+header("Last-Modified: $time");
+
+$cur = $begin;
+fseek($fm, $begin, 0);
+
+while(!feof($fm) && $cur <= $end && (connection_status() == 0)) {
+	print fread($fm, min(1024 * 16, ($end - $cur) + 1));
+	$cur += 1024 * 16;
+}
--- index.php
+++ index.php
@@ -21,8 +21,8 @@
 	<a href='#' onclick='javascript:playhtml5sound("podcast/bunny.mp3");return false;'>mp3</a> | 
 	<a href='#' onclick='javascript:playhtml5sound("podcast/bunny.m4a");return false;'>m4a</a><br />
 	<strong>Direct linked:</strong>
-	<a href='#' onclick='javascript:playhtml5sound("bunny.mp3");return false;'>mp3</a> | 
-	<a href='#' onclick='javascript:playhtml5sound("bunny.m4a");return false;'>m4a</a><br />
+	<a href='#' onclick='javascript:playhtml5sound("dl.php?file=bunny.mp3&type=mpeg");return false;'>mp3</a> | 
+	<a href='#' onclick='javascript:playhtml5sound("dl.php?file=bunny.m4a&type=mp4");return false;'>m4a</a><br />
 
 	<script>	  
 		soundManager.setup({
@@ -32,13 +32,13 @@
 		    onready: function() {
 				soundManager.createSound({
 					id: 'phpmp3',
-					url: 'podcast/bunny.mp3',
+					url: 'dl.php?file=bunny.mp3&type=mpeg',
 					volume: 50
 				});
 
 				soundManager.createSound({
 					id: 'phpm4a',
-					url: 'podcast/bunny.m4a',
+					url: 'dl.php?file=bunny.m4a&type=mp4',
 					volume: 50
 				});
 
